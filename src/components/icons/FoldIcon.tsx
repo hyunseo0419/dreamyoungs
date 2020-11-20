9402edc090b395fb741341bbe83dcfa0654dac78
hyunseo0419 <wjsdbtjs131313@gmail.com>
2020-11-20 21:00:03 +0900

    Add svg icon components

---
+++ src/components/icons/FoldIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const FoldIcon = () => {
+    return (
+        <svg width="5" height="9" viewBox="0 0 5 9" fill="none" xmlns="http://www.w3.org/2000/svg" >
+<path d="M1.07324e-07 9L0 5.96244e-08L5 4.5L1.07324e-07 9Z" fill="#333333"/>
+</svg>
+
+    )
+}
