ee64305c4015916a0bc0a78942bfb41cac815f80
hyunseo0419 <wjsdbtjs131313@gmail.com>
2020-11-20 21:04:27 +0900

    Update drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {Group} from 'components/Group'
+import ListData,{GroupType} from 'data'
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const App =()=> {
+
+  const [isGroup, setIsGroup] = useState<boolean>(false)
+  const [list,setList] = useState<GroupType[]>(ListData)
+
+useEffect(() => {
+  const unknownGroupIndex =  ListData.findIndex((group:GroupType)=>
+    group.groupUid===null
+  )
+  let clone = [...ListData]
+  clone.splice(unknownGroupIndex,1)
+  clone.push(ListData[unknownGroupIndex])
+  setList(clone)
+}, [])
+
+  const updateList = (newList:GroupType[]) =>{
+    setList(newList)
+  }
+
+  const checkIsGroup = (status:boolean) =>{
+    setIsGroup(status)
+  }
+
   return (
-    <div >
-    
-    </div>
+    <Container >
+      {list.map((group:GroupType)=> (
+        <Group key={group.groupUid} group={group} list={list} setList={updateList} isGroup={isGroup} setIsGroup={checkIsGroup}/>
+      ))}
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+padding: 50px 20px;
+`
